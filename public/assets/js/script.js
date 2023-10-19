89811566aca752abdc714d74268acb2140fef89e
NicolasDona <nicolas.donascimento@gmail.com>
2023-10-19 09:44:12 +0200

    fonction de calcul

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -3,3 +3,15 @@
 // Partie Cédric
 
 // Parie Nicolas
+
+let result = [1, 1, 1, 1, 1];
+console.log(result);
+
+function totalNumber(arr) {
+    let sum = 0;
+    for (let index = 0; index < arr.length; index++) {
+        sum += arr[index];
+    }
+    return sum;
+}
+console.log(totalNumber(result));
