bfd01e3decdbfefff5794647d2e955cf4a58e902
NicolasDona <nicolas.donascimento@gmail.com>
2023-10-23 13:35:21 +0200

    full

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -18,9 +18,9 @@ function displayDice() {
     diceValues.forEach((die, index) => {
         const dieElement = document.createElement('img');
         dieElement.className = 'img-fluid col-2 col-lg-4 mb-3';
-        
+
         dieElement.src = `./public/assets/img/${die.value}.png`;
-        
+
         if (die.selected) {
             dieElement.classList.add('selected');
             dieElement.addEventListener('click', () => toggleSelectDie(index));
@@ -38,7 +38,7 @@ function rollDice() {
             die.value = Math.floor(Math.random() * 6) + 1;
         }
     });
-    
+
     displayDice();
     console.log('diceValues:', diceValues);
     console.log('diceKept:', diceKept);
@@ -46,7 +46,7 @@ function rollDice() {
 
 function toggleSelectDie(index) {
     const die = diceValues[index];
-    
+
     if (die.selected) {
         die.selected = false;
         const selectedIndex = diceKept.indexOf(die.value);
@@ -75,61 +75,59 @@ function totalNumber(des) {
 }
 console.log(totalNumber(diceKept));
 // fonction de comparaison/règles
-const checkRules = (diceKept, rule) =>{
+const checkRules = (diceKept, rule) => {
     let total = 0
-switch (rule) {
-    case 'total1':
-        const ones = diceKept.filter(die => die === 1);
-        if (diceKept.includes(1)) {
-            total = ones.reduce((total, die) => total + die, 0);
-        } else {
-            total = 0;
-        }
-        //condition ternaire
-        // diceKept.includes(1) ? total = ones.reduce((total, die) => total + die, 0) :  total = 0;
-        break;
-    case 'total2':
-        if (diceKept.includes(2)) {
-            const ones = diceKept.filter(die => die === 2);
-            total = ones.reduce((total, die) => total + die, 0);
-            
-        } else {
-            total = 0;
-        }
-        break;
-    case 'total3':
-        if (diceKept.includes(3)) {
-            const ones = diceKept.filter(die => die === 3);
-            total = ones.reduce((total, die) => total + die, 0);
-        } else {
-            total = 0;
-        }
-        break;
-    case 'total4':
-        if (diceKept.includes(4)) {
-            const ones = diceKept.filter(die => die === 4);
-            total = ones.reduce((total, die) => total + die, 0);
-
-        } else {
-            total = 0;
-        }
-        break;
-    case 'total5':
-        if (diceKept.includes(5)) {
-            const ones = diceKept.filter(die => die === 5);
-            total = ones.reduce((total, die) => total + die, 0);
-        } else {
-            total = 0;
-        }
-        break;
-    case 'total6':
-        if (diceKept.includes(6)) {
-            const ones = diceKept.filter(die => die === 6);
-            total = ones.reduce((total, die) => total + die, 0);
-        } else {
-            total = 0
-        }
-        break;
+    switch (rule) {
+        case 'total1':
+            const ones = diceKept.filter(die => die === 1);
+            if (diceKept.includes(1)) {
+                total = ones.reduce((total, die) => total + die, 0);
+            } else {
+                total = 0;
+            }
+            //condition ternaire
+            // diceKept.includes(1) ? total = ones.reduce((total, die) => total + die, 0) :  total = 0;
+            break;
+        case 'total2':
+            if (diceKept.includes(2)) {
+                const ones = diceKept.filter(die => die === 2);
+                total = ones.reduce((total, die) => total + die, 0);
+            } else {
+                total = 0;
+            }
+            break;
+        case 'total3':
+            if (diceKept.includes(3)) {
+                const ones = diceKept.filter(die => die === 3);
+                total = ones.reduce((total, die) => total + die, 0);
+            } else {
+                total = 0;
+            }
+            break;
+        case 'total4':
+            if (diceKept.includes(4)) {
+                const ones = diceKept.filter(die => die === 4);
+                total = ones.reduce((total, die) => total + die, 0);
+            } else {
+                total = 0;
+            }
+            break;
+        case 'total5':
+            if (diceKept.includes(5)) {
+                const ones = diceKept.filter(die => die === 5);
+                total = ones.reduce((total, die) => total + die, 0);
+            } else {
+                total = 0;
+            }
+            break;
+        case 'total6':
+            if (diceKept.includes(6)) {
+                const ones = diceKept.filter(die => die === 6);
+                total = ones.reduce((total, die) => total + die, 0);
+            } else {
+                total = 0
+            }
+            break;
         case 'brelan':
             for (let index = 0; index < diceKept.length - 2; index++) {
                 if (diceKept[index] === diceKept[index + 1] && diceKept[index] === diceKept[index + 2]) {
@@ -138,62 +136,55 @@ switch (rule) {
                 }
             }
             break;
-            case 'carre':
-                for (let index = 0; index < diceKept.length - 3; index++) {
-                    if (diceKept[index] === diceKept[index + 1] && diceKept[index] === diceKept[index + 2] && diceKept[index] === diceKept[index + 3]) {
-                        total = diceKept[index] * 4;
-                        break;
-                    }
+        case 'carre':
+            for (let index = 0; index < diceKept.length - 3; index++) {
+                if (diceKept[index] === diceKept[index + 1] && diceKept[index] === diceKept[index + 2] && diceKept[index] === diceKept[index + 3]) {
+                    total = diceKept[index] * 4;
+                    break;
                 }
-                break;
+            }
+            break;
         case 'full':
-            let threeNumbers = [];
-            let twoNumbers = [];
-            console.log(threeNumbers, twoNumbers);
-            for (let index = 0; index <= 3; index++) {
-                if (diceKept[index] === diceKept[index + 2]) {
-                    threeNumbers.push(diceKept[index]);
-                }
+            const counts = Array(7).fill(0);  // De 0 à 6  pas d'index 0)
+            for (const die of diceKept) {
+                counts[die]++;
             }
-            for (let index = 0; index <= 2; index++) {
-                if (diceKept[index] === diceKept[index + 1]) {
-                    twoNumbers.push(diceKept[index]);
-                }
-            }
-            if (threeNumbers.length === 3 && twoNumbers.length === 2) {
+            const threeNumber = counts.includes(3);
+            const twoNumber = counts.includes(2);
+            if (threeNumber && twoNumber) {
                 total = 25;
             }
             break;
         case 'petiteSuite':
-            if (diceKept.join('') === '12345') {       
-                total= 30;       
+            if (diceKept.join('') === '12345') {
+                total = 30;
             } else {
                 total = 0
             }
             break;
         case 'grandeSuite':
-            if (diceKept.join('') === '23456') {       
-                total= 40;       
+            if (diceKept.join('') === '23456') {
+                total = 40;
             } else {
                 total = 0
             }
             break;
-            case 'yams':
-                if (new Set(diceKept).size === 1) {  // Tous les dés sont identiques
-                    total = 50;
-                }
-                break;
-        case 'chance':
-            total= totalNumber(diceKept);
+        case 'yams':
+            if (new Set(diceKept).size === 1) {  // Tous les dés sont identiques
+                total = 50;
+            }
             break;
-    default:
-        console.log("opération non reconnue");
-        break;
+        case 'chance':
+            total = totalNumber(diceKept);
+            break;
+        default:
+            console.log("opération non reconnue");
+            break;
     }
-return total
+    return total
 }
 
-const ruleOk =  ['total1','total2','total3','total4','total5','total6', 'brelan', 'carre', 'full', 'petiteSuite', 'grandeSuite', 'yams', 'chance'];
+const ruleOk = ['total1', 'total2', 'total3', 'total4', 'total5', 'total6', 'brelan', 'carre', 'full', 'petiteSuite', 'grandeSuite', 'yams', 'chance'];
 
 ruleOk.forEach(rule => {
     const score = checkRules(diceKept, rule);
